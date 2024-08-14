22bd1336e975f1060c72c3cb981b7856ad89de2d
Gk070 <geethulkrishna@gmail.com>
2024-08-14 10:40:20 +0530

    .

--- MySQL Local.session.sql
+++ MySQL Local.session.sql
@@ -12,11 +12,18 @@ CREATE TABLE Employee_Demo_Graphics(
     Gender VARCHAR(6)
 );
 
+CREATE TABLE Parks_Departments(
+	Department_ID INT PRIMARY KEY,
+    Department_Name VARCHAR(20)
+);
+
 CREATE TABLE Employee_Salary(
     Employee_ID INT,
     Job_Title VARCHAR(15), 
     Salary INT, 
-    FOREIGN KEY (Employee_ID) REFERENCES Employee_Demo_Graphics(Employee_ID) ON DELETE CASCADE
+    Department_ID INT,
+    FOREIGN KEY (Employee_ID) REFERENCES Employee_Demo_Graphics(Employee_ID) ON DELETE CASCADE,
+    FOREIGN KEY (Department_ID) REFERENCES Parks_Departments(Department_ID) ON DELETE CASCADE
 );
 
 INSERT INTO Employee_Demo_Graphics VALUES
@@ -31,22 +38,47 @@ INSERT INTO Employee_Demo_Graphics VALUES
     (9, 'Michael', 'Martinez', 31, 'Male'),
     (10, 'Olivia', 'Lopez', 26, 'Female');
 
+INSERT INTO Parks_Departments VALUES 
+	(1, 'Maintenance'),
+	(2, 'Recreation'),
+	(3, 'Events'),
+	(4, 'Conservation'),
+	(5, 'Administration'),
+	(6, 'Public Relations');
+
 INSERT INTO Employee_Salary VALUES
-    (1, 'Engineer', 75000),
-    (2, 'Analyst', 65000),
-    (3, 'Manager', 85000),
-    (4, 'Engineer', 78000),
-    (5, 'Technician', 55000),
-    (6, 'Analyst', 68000),
-    (7, 'Director', 95000),
-    (8, 'Engineer', 72000),
-    (9, 'Manager', 83000),
-    (10, 'Technician', 57000);
+    (1, 'Engineer', 75000, 1),
+    (2, 'Analyst', 65000, 6),
+    (3, 'Manager', 85000, 3),
+    (4, 'Engineer', 78000, 5),
+    (5, 'Technician', 55000, 1),
+    (6, 'Analyst', 68000, 6),
+    (7, 'Director', 95000, 3),
+    (8, 'Engineer', 72000, 2),
+    (9, 'Manager', 83000, 3),
+    (10, 'Technician', 57000, 1);
 
-/* Inner Join (It creates a new table by combining rows that have matching values in two or more tables)*/
+/* Inner Join/ Join(It creates a new table by combining rows that have matching values in two or more tables)*/
 
-SELECT * FROM Employee_Demo_Graphics EDG INNER JOIN Employee_Salary ES ON EDG.Employee_ID = ES.Employee_ID;
+SELECT * FROM Employee_Demo_Graphics EDG JOIN Employee_Salary ES ON EDG.Employee_ID = ES.Employee_ID;
+SELECT EDG.Employee_ID, Age, ES.Job_Title FROM Employee_Demo_Graphics EDG JOIN Employee_Salary ES ON EDG.Employee_ID = ES.Employee_ID;
 
-/* Full Outer Join */
+/* Left Join(The LEFT JOIN keyword returns all records from the left table, even if there are no matches in the right table) */
 
-SELECT * FROM Employee_Demo_Graphics EDG FULL OUTER JOIN Employee_Salary ES ON EDG.Employee_ID = ES.Employee_ID;
+SELECT * FROM Employee_Demo_Graphics EDG LEFT JOIN Employee_Salary ES ON EDG.Employee_ID = ES.Employee_ID;
+
+/* Right Join(The Right JOIN keyword returns all records from the right table, even if there are no matches in the left table) */
+
+SELECT * FROM Employee_Demo_Graphics EDG RIGHT JOIN Employee_Salary ES ON EDG.Employee_ID = ES.Employee_ID;
+
+/* Self Join(It is a regular join that is used to join a table with itself) */
+
+SELECT * FROM  Employee_Salary ES1 JOIN Employee_Salary ES2 ON ES1.Employee_ID + 1 = ES2.Employee_ID;
+
+/* Multiple Join */
+
+SELECT * FROM Employee_Demo_Graphics EDG JOIN Employee_Salary ES ON EDG.Employee_ID = ES.Employee_ID JOIN Parks_Departments PD ON ES.Department_ID = PD.Department_ID;
+
+/* UNION(Removes Duplicate (UNION ALL is used if we want duplicate data)) */
+
+SELECT Age, Gender FROM Employee_Demo_Graphics UNION SELECT Salary, Job_Title FROM Employee_Salary;
